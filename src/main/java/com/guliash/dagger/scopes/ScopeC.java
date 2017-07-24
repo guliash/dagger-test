93fa2071b1635445dc94328ec467c0a0757ea838
Artem Gilmudinov <guliash92@gmail.com>
2017-07-24 14:33:32 +0300

    init;

---
+++ src/main/java/com/guliash/dagger/scopes/ScopeC.java
@@ -0,0 +1,10 @@
+package com.guliash.dagger.scopes;
+
+import javax.inject.Scope;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ScopeC {
+}
